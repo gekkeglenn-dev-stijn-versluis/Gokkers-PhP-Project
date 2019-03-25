50c1dbe09ca7a88c50d3813710943487e3a608f9
stijn versluis <stijn.versluis@gmail.com>
2019-03-25 15:59:21 +0100

    login en register CSS

--- login.php
+++ login.php
@@ -1,18 +1,18 @@
 <?php require 'header.php'; ?>
 
-<form action="includes/loginController.php" method="post">
+<form class="login_form" action="includes/loginController.php" method="post">
     <input type="hidden" name="type" value="login">
     <div class="form-group">
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email" required>
+        <label class="email_label" for="email">Email</label>
+        <input class="email_input" type="text" name="email" id="email" required>
     </div>
 
     <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required>
+        <label class="password_label" for="password">Password</label>
+        <input class="password_input" type="password" name="password" id="password" required>
     </div>
 
-    <input type="submit" value="login">
+    <input class="login_submit" type="submit" value="login">
 </form>
 
 <?php require 'footer.php'; ?>
--- register.php
+++ register.php
@@ -1,33 +1,33 @@
 <?php require 'header.php'; ?>
 
-<form action="includes/loginController.php" method="post">
+<form class="register_form" action="includes/loginController.php" method="post">
     <input type="hidden" name="type" value="register">
     <div class="form-group">
-        <label for="username">username</label>
-        <input type="text" name="username" id="username" required>
+        <label class="username_label" for="username">username</label>
+        <input class="username_input" type="text" name="username" id="username" required>
     </div>
 
     <div class="form-group">
-        <label for="email">email</label>
-        <input type="email" name="email" id="email" required>
+        <label class="email_label" for="email">email</label>
+        <input class="email_input" type="email" name="email" id="email" required>
     </div>
 
     <div class="form-group">
-        <label for="password">password</label>
-        <input type="password" name="password" id="password" required>
+        <label class="password_label" for="password">password</label>
+        <input class="password_input" type="password" name="password" id="password" required>
     </div>
 
     <div class="form-group">
-        <label for="password_confirm">Please confirm your password</label>
-        <input type="password" name="password_confirm" id="password_confirm" required>
+        <label class="password_label" for="password_confirm">Please confirm your password</label>
+        <input class="password_input" type="password" name="password_confirm" id="password_confirm" required>
     </div>
 
     <div class="from-group">
-        <input type="checkbox" name="algemene-voorwaarde" id="algemene-voorwaarde" required>
+        <input class="voorwaarden_checkbox" type="checkbox" name="algemene-voorwaarde" id="algemene-voorwaarde" required>
         <p>Gaat akkoort met de <a href="#">algemen voorwaarden</a></p>
     </div>
 
-    <input type="submit" value="Register">
+    <input class="register_submit" type="submit" value="Register">
 </form>
 
 <?php require 'footer.php'; ?>
