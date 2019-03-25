fe82f8e7ababfcf378926840f5cf8ab60bb1e776
stijn versluis <stijn.versluis@gmail.com>
2019-03-25 16:15:14 +0100

    adminLink

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 <div class="text">
    <h1>Welcome to AMO Login system Home Page! </h1>
    <p>Nothing special to see here but that's not the point.</p>
-   <p>Try going to the ultra secret ultimate vip <a href='admin.php'> admin</a> page without logging in first</p>
+   <p>Try going to the ultra secret ultimate vip <a class="admin_btn" href='admin.php'><i class="fas fa-arrow-right"></i> admin <i class="fas fa-arrow-left"></i></a> page without logging in first</p>
 </div>
 <?php require 'footer.php'; ?>
